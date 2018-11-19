5ae4062d07f26038cf876746b89912ff51a87dff
dmsansan <562404326@qq.com>
2018-11-19 15:08:48 +0800

    修改mapper扫描范围

--- modules/activiti-spring-boot-test/src/main/java/neusoft/activiti/com/Application.java
+++ modules/activiti-spring-boot-test/src/main/java/neusoft/activiti/com/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan(basePackages = "neusoft.activiti.com.dao")
-@ComponentScan(basePackages = "org.activiti.engine")
 @ConfigurationProperties
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
