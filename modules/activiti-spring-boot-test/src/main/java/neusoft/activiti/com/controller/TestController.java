3126bf3e4457b480e28b52b10d6d5561c665e4ce
dmsansan <562404326@qq.com>
2018-11-19 15:00:44 +0800

    添加flyway数据库版本管理工具

--- modules/activiti-spring-boot-test/src/main/java/neusoft/activiti/com/controller/TestController.java
+++ modules/activiti-spring-boot-test/src/main/java/neusoft/activiti/com/controller/TestController.java
@@ -2,8 +2,6 @@ package neusoft.activiti.com.controller;
 
 import neusoft.activiti.com.entity.User;
 import neusoft.activiti.com.service.UserService;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.impl.persistence.entity.TestUserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,20 +16,14 @@ public class TestController {
     @Autowired
     private UserService userService;
 
-    private RepositoryService repositoryService;
-
     @RequestMapping("/index")
     public String index(){
         return "Spring boot test!!!";
     }
 
     @RequestMapping("/getUser")
-    public User selectByPrimaryKey(HttpServletRequest request){
-        TestUserEntity testUserEntity = repositoryService.selectByPrimaryKey(
-                Integer.parseInt(request.getParameter("id")));
-        System.out.print("-------------------------"+testUserEntity);
+        public User selectByPrimaryKey(HttpServletRequest request){
        User user = new User();
-        user.setUsername(testUserEntity.getUserName());
         return user;
     }
 }
